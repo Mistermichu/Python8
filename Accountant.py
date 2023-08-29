e4e90c557b22d0a4b670b15d25d8ebf852936845
MichałM <michalhm@gmail.com>
2023-08-29 21:50:06 +0200

    Execute but with bugs

--- Accountant.py
+++ Accountant.py
@@ -3,53 +3,72 @@ from functions import menu, balance, account_balance_note, sell, buy, list_overv
 from Manager import Manager
 
 manager = Manager()
+
+
+@manager.assign("balance")
+def manage_balance(manager):
+    account_balance += balance(history)
+    account_balance_note(account_balance)
+
+
+@manager.assign("sell")
+def manage_sell(manager):
+    account_balance += sell(history, inventory)
+    account_balance_note(account_balance)
+
+
+@manager.assign("buy")
+def manage_buy(manager):
+    account_balance -= buy(account_balance, history, inventory)
+    account_balance_note(account_balance)
+
+
+@manager.assign("account_balance")
+def manage_account_balance(manager):
+    account_balance_note(account_balance)
+
+
+@manager.assign("list")
+def manage_list_overview(manager):
+    list_overview(inventory)
+
+
+@manager.assign("inventory")
+def manage_inventory_overview(manager):
+    inventory_overview(inventory)
+
+
+@manager.assign("history")
+def manage_history_overview(manager):
+    history_overview(history)
+
+
+@manager.assign("inventory_correction")
+def manage_inventory_correction(manager):
+    inventory_correction(history, inventory)
+
+
+@manager.assign("exit")
+def manage_exit(manager):
+    global run
+    run = False
+    save_data = FileWriter(
+        "history.txt", "balance.txt", "inventory.json")
+    save_data.save_history(history)
+    save_data.save_balance(account_balance)
+    save_data.save_inventory(inventory)
+
+
+# Run app
 run = True
 load_data = FileHandler("history.txt", "balance.txt", "inventory.json")
 history = load_data.history
 account_balance = load_data.account_balance
 inventory = load_data.inventory
 while run:
-    command_check = True
-    while command_check:
-        menu()
-        try:
-            command = int(input(": "))
-            if command == 1:
-                account_balance += balance(history)
-                account_balance_note(account_balance)
-                command_check = False
-            elif command == 2:
-                account_balance += sell(history, inventory)
-                account_balance_note(account_balance)
-                command_check = False
-            elif command == 3:
-                account_balance -= buy(account_balance, history, inventory)
-                account_balance_note(account_balance)
-                command_check = False
-            elif command == 4:
-                account_balance_note(account_balance)
-                command_check = False
-            elif command == 5:
-                list_overview(inventory)
-                command_check = False
-            elif command == 6:
-                inventory_overview(inventory)
-                command_check = False
-            elif command == 7:
-                history_overview(history)
-                command_check = False
-            elif command == 8:
-                inventory_correction(history, inventory)
-                command_check = False
-            elif command == 9:
-                command_check = False
-                run = False
-                save_data = FileWriter(
-                    "history.txt", "balance.txt", "inventory.json")
-                save_data.save_history(history)
-                save_data.save_balance(account_balance)
-                save_data.save_inventory(inventory)
-            else:
-                bad_response()
-        except ValueError:
-            bad_response()
+    menu()
+    try:
+        command = int(input(": "))
+        manager.execute(command)
+    except ValueError:
+        bad_response()
